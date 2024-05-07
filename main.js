6361dd0a39332e804d69d820f636df6a1376af40
YVONNE NJOROGE <yvonnenjoroge46@gmail.com>
2024-05-07 11:36:37 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+// Select the button element
+let button = document.querySelector('.cta-button');
+
+// Attach an event listener to the button
+button.addEventListener('click', () => {
+    // Display an alert message when the button is clicked
+    alert('Thank you for your interest! Explore our services.');
+});
